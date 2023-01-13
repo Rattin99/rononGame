b195193458e3a61b1d0fc8fa043743bf68fdd1c3
Rattin99 <rattin99m18@gmail.com>
2023-01-14 03:47:16 +0600

    phaser

--- game.js
+++ game.js
@@ -1,135 +1,59 @@
+const height = window.innerHeight;
+const width = window.innerWidth;
 
-
-kaboom({
-    global: true,
-    scale:1,
-    debug: true,
-    clearColor: [0,0,0,1],
-    orientation: true,
-})
-
-loadSprite("pc","sprites/Cam.png")
-
-function spawnObs(score, time) {
-
-    if(score < 100 && time % 300 === 0){
-        add([
-            rect(48,rand(24,64)),
-            area(),
-            outline(4),
-            pos(width(),height() - 48),
-            origin("botleft"),
-            move(LEFT,240),
-            color(255,180,255),
-            "obs",
-            cleanup()
-        ])
+var config = {
+    type: Phaser.AUTO,
+    width: width,
+    height: height,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create
     }
+};
 
-    if(score < 300 && score > 100 && time % 200 === 0){
-        add([
-            rect(48,rand(24,64)),
-            area(),
-            outline(4),
-            pos(width(),height() - 48),
-            origin("botleft"),
-            move(LEFT,240),
-            color(255,180,255),
-            "obs",
-            cleanup()
-        ])
-    }
+var game = new Phaser.Game(config);
 
-    if(score < 500 && score > 300 && time % 100 === 0){
-         add([
-            rect(48,rand(24,64)),
-            area(),
-            outline(4),
-            pos(width(),height() - 48),
-            origin("botleft"),
-            move(LEFT,240),
-            color(255,180,255),
-            "obs",
-            cleanup()
-        ])
-    }
+function preload ()
+{
+    // this.load.setBaseURL('http://labs.phaser.io');
 
-    if(score > 500 && time % 50 === 0) {
-        add([
-            rect(48,rand(24,64)),
-            area(),
-            outline(4),
-            pos(width(),height() - 48),
-            origin("botleft"),
-            move(LEFT,240),
-            color(255,180,255),
-            "obs",
-            cleanup()
-        ])
-    }
+    this.load.image('nongar',"sprites/nongar.jpg")
+    this.load.image('chess',"sprites/chess.jpg")
 
-   
+    this.load.image('sky', 'assets/skies/space3.png');
+    this.load.image('ground', 'http://labs.phaser.io/assets/sprites/platform.png');
+    this.load.image('red', 'assets/particles/red.png');
 }
 
-scene("game",()=> {
+let platforms;
 
-const pc = add([
-    sprite("pc"),
-    pos(300,300),
-    scale(0.03),
-    color(0,0,255),
-    area(),
-    body()
-])
+function create ()
+{
+    
 
 
-onKeyPress("space", () => {
-    if(pc.isGrounded()){
-        pc.jump();
-    }
-})
+   const nongar =  this.add.image(400, 300, 'nongar');
 
-add([
-    rect(width(),48),
-    pos(0,height() - 48),
-    outline(4),
-    area(),
-    solid(),
-    color(127,200,255)
-])
+   nongar.setScale(0.5)
 
-pc.onCollide("obs",() => {
-    addKaboom(pc.pos);
-    shake();
-    go("lose")    
-})
+    var chess = this.physics.add.image(400, 100, 'chess');
 
-let score = 0;
-let time = 0;
-const scoreLabel = add([
-    text(score),
-    pos(24,24)
-])
+    platforms = this.physics.add.staticGroup();
 
-onUpdate(()=>{
-    score += 0.1;
-    time++;
-    const score_floor = Math.floor(score);
-    scoreLabel.text = score_floor;
-
-    spawnObs(score_floor,time)
-})
+    platforms.create(400,568,'ground').setScale(2).refreshBody();
 
 
+    chess.setScale(0.1)
 
-})
+    chess.setVelocity(100, 200);
+    chess.setBounce(1, 1);
+    chess.setCollideWorldBounds(true);
 
-scene("lose",()=> {
-    add([
-        text("Game Over"),
-        pos(center()),
-        origin("center")
-    ])
-})
-
-go("game")
+    this.physics.add.collider(chess,platforms)
+}
